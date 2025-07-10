12477484684325d17ce7ccfe01579fadc7bbcc13
Polo9422 <polo9422@gmail.com>
2025-07-10 10:05:33 -0300

    subo cambios

--- Libro Javascript/Libro3Funciones.js
+++ Libro Javascript/Libro3Funciones.js
@@ -123,7 +123,21 @@ function imprimirInventarioGranja(vacas, pollos){
     console.log(`${stringPollos} Polllos`);
 }
 imprimirInventarioGranja(7,11);
-
+console.log(10);
+//Extendemos la funcion para CERDOS
+function imprimirEtiquetaAlcochadaConCeros(numero, etiqueta){
+    let stringNumero = String(numero);
+    while (stringNumero.length < 3){
+        stringNumero= "0" + stringNumero;
+    }
+    console.log(`${stringNumero} ${etiqueta}`);
+}
+function imprimirInventarioGranja(vacas, pollos, cerdos){
+imprimirEtiquetaAlcochadaConCeros(vacas, "Vacas");
+imprimirEtiquetaAlcochadaConCeros(pollos, "Pollos");
+imprimirEtiquetaAlcochadaConCeros(cerdos, "Cerdos");
+}
+imprimirInventarioGranja(7,11,3);
 
 //resultado *= base; ***ES IGUAL*** resultado = resultado * base;
  
